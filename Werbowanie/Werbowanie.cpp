9c20d2059afba5c47c114dfa2bce5d3404aa5bae
Mateusz Piotr Moruś <mpmorus@gmail.com>
2019-10-11 12:45:27 +0200

    Zadanie 2 - werbowanie - zaliczone

--- Werbowanie/Werbowanie.cpp
+++ Werbowanie/Werbowanie.cpp
@@ -2,18 +2,30 @@
 #include <string>
 
 typedef unsigned short ushort;
-typedef unsigned int uint;
 typedef unsigned long long int ullint;
 
 struct person {
 	std::string name;
-	ullint time;
+	int time;
 	bool operator > (const person& other) const {
 		if (this->time < other.time) return true;
-		else if (this->time == other.time && this->name > other.name) return true;
-		return false;
+		else if (this->time > other.time) return false;
+		else if (this->name > other.name) return true;
+		else return false;
 	}
 };
+
+void insertion_sort(person* people, ushort n) {
+	for (ushort i = 1; i < n; ++i) {
+		person focus_point = people[i];
+		int j = i - 1;
+		while (j >= 0 && people[j] > focus_point) {
+			people[j + 1] = people[j];
+			--j;
+		}
+		people[j + 1] = focus_point;
+	}
+}
 
 int main() {
 	ushort t;
@@ -26,34 +38,26 @@ int main() {
 		ushort p; // Liczna nazwisk do wyœwietlenia
 		std::cin >> n >> p;
 		person* people = new person[n]; // Struktury zawieraj¹ce dane o osobach
-		ushort* indexes = new ushort[n]; // Indeksy
 		for (ushort i = 0; i < n; ++i) {
 			std::cin >> people[i].name >> people[i].time;
-			indexes[i] = i;
 		}
 
 		/* Insertion sort */
 
-		for (ushort i = 1; i < n; ++i) {
-			uint focus_point = indexes[i];
-			short j = i - 1;
-			while (j >= 0 && people[indexes[j]] > people[focus_point]) {
-				indexes[j + 1] = indexes[j];
-				--j;
-			}
-			indexes[j + 1] = focus_point;
-		}
+		insertion_sort(people, n);
 
 		/* Wyœwietlenie */
 		for (ushort i = 0; i < p; ++i) {
-			std::cout << people[indexes[i]].name << " ";
+			std::cout << people[i].name << " ";
 		}
 		ullint sum = n;
 		for (ushort i = 1; i < n; ++i) {
-			sum += people[indexes[i]].time * i;
+			sum += (ullint)people[i].time * i;
 			sum %= 1000003;
 		}
 		std::cout << std::endl << sum << std::endl;
+
+		delete[] people;
 	}
 	return 0;
 }
