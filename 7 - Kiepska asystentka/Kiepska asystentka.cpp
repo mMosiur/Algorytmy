56571df7921872e1d00ba40649c3355d5ff3f8ea
Mateusz Piotr Moruś <mpmorus@gmail.com>
2019-11-25 20:06:22 +0100

    Algorytm 7 [11/11]

--- 7 - Kiepska asystentka/Kiepska asystentka.cpp
+++ 7 - Kiepska asystentka/Kiepska asystentka.cpp
@@ -1,63 +1,72 @@
 #include <iostream>
+#include <sstream>
 #include <string>
-#include <map>
 #include <vector>
+#include <map>
 
 typedef unsigned short ushort;
 typedef unsigned int uint;
 
+// Vertex representing a person, contatining information about its neighbors
 struct Vertex {
-	std::string name;
-	uint nof_incoming_edges = 0;
-	std::vector<Vertex*> outgoing_edges;
-
-	Vertex(std::string name) : name(name) {}
-
-	void add_edge(Vertex* to) {
-		outgoing_edges.push_back(to);
-		to->nof_incoming_edges++;
-	}
+	uint nof_incoming_edges = 0; // The number of vertices thath should be before this one
+	std::vector<Vertex*> outgoing_edges; // Vertices that should be after this one
 };
 
+// Graph representing reations between people
 class Graph {
-	std::map<std::string, Vertex*> vertices;
-
+	std::map<std::string, Vertex*> vertices; // The map of vertices, where the name of a person is the key to vertex that describes it
+	std::stringstream path; // The stream that contains the path from the assistant
 public:
+
+	// Constructor, loads all needed data
 	Graph(uint nof_vertices, uint nof_edges) {
 		// Load people into the graph
 		while (nof_vertices-- > 0) {
 			std::string name;
 			std::cin >> name;
-			vertices[name] = new Vertex(name);
+			vertices[name] = new Vertex;
 		}
 		// Load connections into the graph
 		while (nof_edges-- > 0) {
 			std::string from, to;
 			std::cin >> from >> to;
-			Vertex* first = vertices[from];
-			Vertex* second = vertices[to];
-			first->add_edge(second);
+			add_edge(vertices[from], vertices[to]);
+		}
+		// Load the assistant's path
+		std::string path_str;
+		std::cin.get(); // Get rid of the newline in the buffor
+		std::getline(std::cin, path_str);
+		path.str(path_str);
+	}
+
+	// Descrutor which deletes all vertices of the graph upon deletion
+	~Graph() {
+		for (auto& vertex : vertices) {
+			delete vertex.second;
 		}
 	}
 
+	// The funtion that checks and returns wether the path in this graph is correct
 	bool is_path_correct() {
-		uint nof_vertices = vertices.size();
-		while (nof_vertices-- > 0) {
+		while (vertices.size() > 0) {
 			std::string name;
-			std::cin >> name;
+			path >> name;
 			Vertex* person = vertices[name];
+			if (person->nof_incoming_edges > 0) return false;
 			vertices.erase(name);
-			if (person->nof_incoming_edges != 0) return false;
-			for (auto& v : person->outgoing_edges) v->nof_incoming_edges--;
+			for (auto& vertex : person->outgoing_edges) {
+				vertex->nof_incoming_edges--;
+			}
 			delete person;
 		}
 		return true;
 	}
 
-	~Graph() {
-		for (auto& vertex : vertices) {
-			delete vertex.second;
-		}
+	// Adds an edge in the graph between two vertices
+	void add_edge(Vertex* from, Vertex* to) {
+		from->outgoing_edges.push_back(to);
+		to->nof_incoming_edges++;
 	}
 };
 
@@ -68,13 +77,13 @@ int main() {
 	std::cin >> t;
 
 	while (t-- > 0) {
-		uint n, m;
+		uint n; // The number of people (vertices)
+		uint m; // The number of pairs describing path's order (edges)
 		std::cin >> n >> m;
 
-		Graph graph(n, m);
+		Graph graph(n, m); // The graph representing relations between people in this data set
 
 		std::cout << (graph.is_path_correct() ? "TAK" : "NIE") << std::endl;
-
 	}
 
 	return 0;
